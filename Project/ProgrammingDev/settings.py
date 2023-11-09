28a2f130d23945494dc99174cfdd63d4c3a521be
AdrianContre <adrian.contreras.martin@gmail.com>
2023-11-09 12:43:12 +0100

    config

--- Project/ProgrammingDev/settings.py
+++ Project/ProgrammingDev/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'g1o--qykz&0k*j_#-a%xh-!+9o%ms0$hm^7mg@oln@(7#wfu2!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
